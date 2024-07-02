16feaa91fbb1b885c1988a3de9389b7ede03ca22
chen-chen-stat <chen.chen.stat@outlook.com>
2024-07-02 08:21:03 +0000

    Updates styling and wordlist

--- R/stacked_barchart.R
+++ R/stacked_barchart.R
@@ -13,19 +13,19 @@
 #'   data = comp_outcome,
 #'   chartcolors = colfun()$fig12_colors,
 #'   xlabel = "Study Week"
-#'  )
+#' )
 #'
 #' # unequal number of subjects across treatments
 #' comp_outcome2 <- comp_outcome[
-#'   (comp_outcome$trtn == 1 & comp_outcome$usubjid %in% c(1:40))
-#'   | (comp_outcome$trtn == 2 & comp_outcome$usubjid %in% c(101:160))
-#'   | (comp_outcome$trtn == 3 & comp_outcome$usubjid %in% c(201:250))
-#' ,]
+#'   (comp_outcome$trtn == 1 & comp_outcome$usubjid %in% c(1:40)) |
+#'     (comp_outcome$trtn == 2 & comp_outcome$usubjid %in% c(101:160)) |
+#'     (comp_outcome$trtn == 3 & comp_outcome$usubjid %in% c(201:250)),
+#' ]
 #' stacked_barchart(
 #'   data = comp_outcome2,
 #'   chartcolors = colfun()$fig12_colors,
 #'   xlabel = "Study Week"
-#'  )
+#' )
 #'
 #' # unequal number of observations across visits
 #' comp_outcome3 <- comp_outcome[!(comp_outcome$trtn == 1 &
@@ -34,9 +34,8 @@
 #'   data = comp_outcome3,
 #'   chartcolors = colfun()$fig12_colors,
 #'   xlabel = "Study Week"
-#'  )
-
-stacked_barchart <- function(data, chartcolors, xlabel = "Visit"){
+#' )
+stacked_barchart <- function(data, chartcolors, xlabel = "Visit") {
   all_columns <- c(
     "usubjid", "visit", "trt", "brcat"
   )
@@ -51,7 +50,7 @@ stacked_barchart <- function(data, chartcolors, xlabel = "Visit"){
     group_by(trt, visit) %>%
     summarise(n1 = n())
 
-  if (nrow(unique(df_n1[c("trt","n1")])) > nrow(unique(df_n1["trt"]))) {
+  if (nrow(unique(df_n1[c("trt", "n1")])) > nrow(unique(df_n1["trt"]))) {
     warning(paste(
       "You have unequal number of observations across visits, please check missing data."
     ))
@@ -59,7 +58,7 @@ stacked_barchart <- function(data, chartcolors, xlabel = "Visit"){
 
   df_n2 <- data %>%
     group_by(trt, visit, brcat) %>%
-    summarise(n2=n())
+    summarise(n2 = n())
 
   df_stacked <- merge(df_n1, df_n2, by = c("trt", "visit"))
   df_stacked$percentage <- df_stacked$n2 / df_stacked$n1 * 100
@@ -68,12 +67,14 @@ stacked_barchart <- function(data, chartcolors, xlabel = "Visit"){
     facet_wrap(~trt, scales = "free_x") +
     geom_bar(stat = "identity", color = "black") +
     scale_fill_manual(values = chartcolors) +
-    geom_text(aes(label = round(percentage, 0)), vjust = 1.3, position = "Stack",
-              size = control_fonts()$p * 0.35) +
+    geom_text(aes(label = round(percentage, 0)),
+      vjust = 1.3, position = "Stack",
+      size = control_fonts()$p * 0.35
+    ) +
     scale_y_continuous(expand = c(0.015, 0)) +
     xlab(xlabel) +
     ylab("Percentage") +
-    guides(fill = guide_legend(title="Outcome", nrow = 2, byrow = TRUE)) +
+    guides(fill = guide_legend(title = "Outcome", nrow = 2, byrow = TRUE)) +
     labs(color = NULL) +
     br_charts_theme(
       panel.grid.major = element_blank(),
--- tests/testthat/test-stacked_barchart.R
+++ tests/testthat/test-stacked_barchart.R
@@ -7,24 +7,24 @@ plot1 <- stacked_barchart(
   data = comp_outcome,
   chartcolors = colfun()$fig12_colors,
   xlabel = "Study Week"
- )
+)
 ggsave(plot1, filename = "test_stacked_barchart1.png", width = 7, height = 5)
 
 test_stacked_barchart1 <- readPNG("test_stacked_barchart1.png")
-stacked_barchart1 <- readPNG(paste0(test_path(),"/snapshots/stacked_barchart1.png"))
+stacked_barchart1 <- readPNG(paste0(test_path(), "/snapshots/stacked_barchart1.png"))
 
 test1 <- paste(100 * sum(test_stacked_barchart1 == stacked_barchart1) /
-                 length(test_stacked_barchart1), "%")
+  length(test_stacked_barchart1), "%")
 expect_equal(test1, "100 %")
 
 file.remove("test_stacked_barchart1.png")
 
 # Example 2 - unequal number of subjects across treatments
 comp_outcome2 <- comp_outcome[
-  (comp_outcome$trtn == 1 & comp_outcome$usubjid %in% c(1:40))
-  | (comp_outcome$trtn == 2 & comp_outcome$usubjid %in% c(101:160))
-  | (comp_outcome$trtn == 3 & comp_outcome$usubjid %in% c(201:250))
-  ,]
+  (comp_outcome$trtn == 1 & comp_outcome$usubjid %in% c(1:40)) |
+    (comp_outcome$trtn == 2 & comp_outcome$usubjid %in% c(101:160)) |
+    (comp_outcome$trtn == 3 & comp_outcome$usubjid %in% c(201:250)),
+]
 plot2 <- stacked_barchart(
   data = comp_outcome2,
   chartcolors = colfun()$fig12_colors,
@@ -33,10 +33,10 @@ plot2 <- stacked_barchart(
 ggsave(plot2, filename = "test_stacked_barchart2.png", width = 7, height = 5)
 
 test_stacked_barchart2 <- readPNG("test_stacked_barchart2.png")
-stacked_barchart2 <- readPNG(paste0(test_path(),"/snapshots/stacked_barchart2.png"))
+stacked_barchart2 <- readPNG(paste0(test_path(), "/snapshots/stacked_barchart2.png"))
 
 test2 <- paste(100 * sum(test_stacked_barchart2 == stacked_barchart2) /
-                 length(test_stacked_barchart2), "%")
+  length(test_stacked_barchart2), "%")
 expect_equal(test2, "100 %")
 
 file.remove("test_stacked_barchart2.png")
@@ -48,19 +48,20 @@ comp_outcome3 <- comp_outcome %>% select(
 
 test_that("test that stacked_barchart returns an error when a required field
           is missing", {
-            expect_error(
-              stacked_barchart(
-                data = comp_outcome3,
-                chartcolors = colfun()$fig12_colors,
-                xlabel = "Study Week"
-              ),
-              "You are missing a required variable in your dataframe: brcat",
-              fixed = TRUE)
-          })
+  expect_error(
+    stacked_barchart(
+      data = comp_outcome3,
+      chartcolors = colfun()$fig12_colors,
+      xlabel = "Study Week"
+    ),
+    "You are missing a required variable in your dataframe: brcat",
+    fixed = TRUE
+  )
+})
 
 # Test if the function returns warning message concerning missing data
 comp_outcome4 <- comp_outcome[!(comp_outcome$trtn == 1 &
-                                  comp_outcome$usubjid %in% c(1:40) & comp_outcome$visit == 5), ]
+  comp_outcome$usubjid %in% c(1:40) & comp_outcome$visit == 5), ]
 
 test_that("test that stacked_barchart returns warning message concerning missing data", {
   expect_warning(stacked_barchart(
@@ -69,4 +70,3 @@ test_that("test that stacked_barchart returns warning message concerning missing
     xlabel = "Study Week"
   ))
 })
-
